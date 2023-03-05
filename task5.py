b116c1f18cd31b354c46d35e934b7a88060d3aaf
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-05 00:26:43 -0800

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,20 @@
+#task 5 last part
+# area of trapezoid
+a=int(input('enter the base of side1:'))
+b=int(input('enter the base of side2:'))
+h=int(input('enter the height of side:'))
+print(1/2*(a+b)*h)
+#task 5 area of parallelogram
+a=int(input('enter the base of paralleogram'))
+b=int(input('enter the height of paralleogram'))
+area=a*b
+print(area)
+#task5 volume of cylinder
+pi=3.14
+r=int(input('enter the radius of cylinder'))
+h=int(input('enter the height of cylinder'))
+v=pi*r**h
+print(v)
+#task surface area of cylinder
+area=2*pi*r*h + 2*pi*r
+print(area)
